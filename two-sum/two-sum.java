014a7c0fc5872a78d43e63ac4fd0f5e513379a9b
Hemansu Kumar Tanty <hemansu.tanty@gmail.com>
2021-08-05 11:08:12 +0530

    Time: 1 ms (99.57%), Space: 39 MB (75.76%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,35 +1,13 @@
-class Number implements Comparable<Number>{
-    public int number;
-    public int index;
-    public Number(int number, int index){
-        this.number=number;
-        this.index=index;
-    }
-    public int compareTo(Number number){
-        return this.number-number.number;
-    }
-}
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        int length = nums.length;
-        Number[] numbers = new Number[length]; 
-        for(int i=0;i<length;i++){
-            numbers[i] = new Number(nums[i], i);
-        }
-        Arrays.sort(numbers);
-        
-        int i = 0, j = length-1;
-        while(i<j){
-            if (numbers[i].number+numbers[j].number==target){
-                result[0]=numbers[i].index;result[1]=numbers[j].index;
-                break;
-            }else if(numbers[i].number+numbers[j].number>target){
-                j--;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{i, map.get(target-nums[i])};
             }else{
-                i++;
+                map.put(nums[i], i);
             }
         }
-        return result;
+        return new int[]{-1,-1};
     }
 }
