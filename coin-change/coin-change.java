dd39558d61af6abddf40f9679ebb3c8daf4ef355
Hemansu Kumar Tanty <hemansu.tanty@gmail.com>
2021-08-05 21:38:26 +0530

    Time: 231 ms (5.03%), Space: 40.3 MB (18.85%) - LeetHub

---
+++ coin-change/coin-change.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        Map<Integer, Integer> dp = new HashMap<>();
+        return coinChange(coins, amount, dp);
+    }
+    
+    public int coinChange(int[] coins, int amount, Map<Integer, Integer> dp){
+        if(amount<0 )return -1;
+        if(amount<1)return 0;
+        if(dp.containsKey(amount)) return dp.get(amount);
+        int minCoins = Integer.MAX_VALUE;
+        for(int coin : coins ){
+            int result = coinChange(coins, amount-coin, dp);
+            if(result>=0 && result < minCoins){
+                minCoins = 1+result;
+                dp.put(amount, minCoins);
+            }
+        }
+        int minCount = minCoins==Integer.MAX_VALUE?-1:minCoins;
+        dp.put(amount,minCount);
+        return dp.get(amount);
+    }
+}
