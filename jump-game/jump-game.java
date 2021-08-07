fed4b34eb147fd506375508339c27123df6137ee
Hemansu Kumar Tanty <hemansu.tanty@gmail.com>
2021-08-07 21:07:13 +0530

    Time: 442 ms (12.42%), Space: 67 MB (5.17%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -1,3 +1,13 @@
+/*
+bool canJump(int A[], int n) {
+    int i = 0;
+    for (int reach = 0; i < n && i <= reach; ++i)
+        reach = max(i + A[i], reach);
+    return i == n;
+}
+
+*/
+
 class Solution {
     public boolean canJump(int[] nums) {
         Boolean[] cache = new Boolean[nums.length];
