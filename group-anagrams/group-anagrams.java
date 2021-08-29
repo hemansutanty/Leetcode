420be3da285580cbdd0fd2dfe60ba781affe7d74
Hemansu Kumar Tanty <hemansu.tanty@gmail.com>
2021-08-29 11:22:16 +0530

    Time: 5 ms (99.14%), Space: 42.6 MB (51.54%) - LeetHub

---
+++ group-anagrams/group-anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] ca = new char[26];
+            for(char ch: s.toCharArray()){
+                ca[ch-'a']++;
+            }
+            String key = String.valueOf(ca);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
